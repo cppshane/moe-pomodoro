9aa748a54fef15a020a532dec4bb7be8ad9605ad
Shane Duffy <shane@shaneduffy.io>
2022-10-21 20:53:16 -0700

    Add image sizing handling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using MoePomodoro;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
